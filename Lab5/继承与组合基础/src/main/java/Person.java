cc94b9e2365f91b649b3164246b670c6a5eee064
shc <3184258272@qq.com>
2026-05-14 14:28:11 +0800

    Save original Person.java

---
+++ Lab5/继承与组合基础/src/main/java/Person.java
@@ -0,0 +1,48 @@
+
+public class Person{
+    public enum  Gender
+    {
+        MALE,FEMALE;
+    }
+    private Name name;
+    private int age;
+    private Gender gender;
+
+    public Person(){
+        this.name = new Name("tomie", "he");
+    }
+    public Person(int a,Gender g,Name n)
+    {
+        this.age=a;
+        this.gender=g;
+        this.name=n;
+    }
+    public Name getName()
+    {
+        return this.name;
+    }
+    public void setName(Name n)
+    {
+        this.name=n;
+    }
+    public void talk()
+    {
+        System.out.println("Hi, how is it going?");
+    }
+    public void talk(String topic)
+    {
+        System.out.println("Let's talk about "+topic);
+    }
+    public void chatWith(Person p,String topic)
+    {
+        String a=this.name.toString();
+        String b=p.name.toString();
+        System.out.printf(a+" to "+b);
+        talk(topic);
+    }
+    @Override
+    public String toString()
+    {
+        return name.toString()+","+age+","+gender;
+    }
+}
